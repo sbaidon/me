27b5ec2aa2859a2de475eca3eb2ae6533ce865fc
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2018-09-08 22:13:30 -0500

    remove unsued script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: 'Sergio Baidon',
   description: 'Software Developer',
-  head: [['script', { src: 'feather-icons/dist/feather.min.js' }]],
   themeConfig: {
     navbar: false,
     search: false,
